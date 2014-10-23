d04de01202bda93afaf98b498b4ebc62d2204546
Legokichi Duckscallion <legokichi@duxca.com>
2014-10-23 23:15:59 +0900

    change interface string to uint8arr

--- test/test.js
+++ test/test.js
@@ -12,31 +12,37 @@ return new Test("URLStorage", {
         worker:     true,
         node:       true,
         button:     true,
-        both:       false, // test the primary module and secondary module
+        both:       true, // test the primary module and secondary module
     }).add([
         test_URLStorage,
     ]).run().clone();
 
 function test_URLStorage(test, pass, miss) {
-    var originalStr = "🍣🍣🍣";
-    for(var i=0;i<10000;i++){ originalStr+=Math.random()*10|0;}
-    new URLStorage().save(originalStr, function(err, shortUrl){
+    var length = 10000;
+    var original = new Uint8Array(length);
+
+    for(var i=0; i < length; i++){
+        original[i] = Math.random()*255|0;
+    }
+
+    new URLStorage().save(original, function(err, shortUrl){
         Valid(Valid.type(err, "Error|null"), test_URLStorage, "err");
         Valid(Valid.type(shortUrl, "String"), test_URLStorage, "shortUrl");
         if(!!err){
             return test.done(miss(err.message));
         }
-        new URLStorage().load(shortUrl, function(err, str){
+        new URLStorage().load(shortUrl, function(err, arr){
             Valid(Valid.type(err, "Error|null"), test_URLStorage, "err");
-            Valid(Valid.type(str, "String"), test_URLStorage, "str");
+            Valid(Valid.type(arr, "Uint8Array"), test_URLStorage, "arr");
             if(!!err){
                 return test.done(miss(err.message));
             }
-            if(originalStr === str){
-                test.done(pass(JSON.stringify(str)));
-            }else{
-                test.done(miss(JSON.stringify(str)));
+            for(var i=0; i < length; i++){
+                if(original[i] !== arr[i]){
+                    return test.done(miss());
+                }
             }
+            test.done(pass());
         });
     });
 }
